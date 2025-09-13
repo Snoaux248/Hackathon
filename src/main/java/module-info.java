df2e91453c68c3b21fc19e7a58adb0953ef2221e
Snow248 <111083139+Snoaux248@users.noreply.github.com>
2025-09-13 13:26:12 -0500

    Pong Update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.project.hackathon.hackathon {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.project.hackathon.hackathon to javafx.fxml;
